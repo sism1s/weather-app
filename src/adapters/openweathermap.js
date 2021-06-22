da8dae8e3a7d234d95bdcb4d61d36f545943564f
sism1s <marko.lazic880@gmail.com>
2021-06-22 14:33:25 +0100

    fetching-data

--- src/adapters/openweathermap.js
+++ src/adapters/openweathermap.js
@@ -2,5 +2,5 @@ import data from "../config/openweathermap.json";
 import axios from "axios";
 
 export const getWeather = () => {
-  return axios.get(`${data.host}?q=${data.city}${data.key}`);
+  return axios.get(`${data.host}?q=${data.city}&appid=${data.key}`);
 };
